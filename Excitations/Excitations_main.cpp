bda8dd7b8d0a7a5adbe97b009d0669300fbb8ddf
Darkdragon84 <stauber.valentin@gmail.com>
2017-11-12 17:32:04 +0100

    split off function to actually calculate excitations for given momenta

--- Excitations/Excitations_main.cpp
+++ Excitations/Excitations_main.cpp
@@ -22,6 +22,24 @@ using IMPSArray = MPSBlockMatArray<IKey,Scalar>;
 using IDiagArray = BlockDiagMatArray<IKey,Scalar>;
 using ILamArray = BlockLamArray<IKey>;
 
+template<typename VT, typename KT, typename HVT>
+void
+CalculateExcitations(RMatType& dE,
+                     std::vector<CMatType>& X,
+                     const MPSBlockMatArray<KT,VT>& ALvec,
+                     const MPSBlockMatArray<KT,VT>& ARvec,
+                     const BlockDiagMatArray<KT,VT>& Lvec,
+                     const BlockDiagMatArray<KT,VT>& Rvec,
+                     SparseOperator<HVT>& H,
+                     const RVecType& pvec,
+                     const KT& K,
+                     uint nev,
+                     Real tol,
+                     Real InvETol,
+                     bool verbose,
+                     bool test=false,
+                     const std::vector<RSpOp>& obs=std::vector<RSpOp>());
+
 int main(int argc, char** argv)
 {
     arma_rng::set_seed_random();
@@ -32,7 +50,7 @@ int main(int argc, char** argv)
     Real tol = 1e-10,InvETol=1e-14;
     uint N=2, nev=4, np=1, rel_shift=0, glob_shift=0;
     Real pmin, pmax;
-    bool verbose=false, saveE=false, saveX=false, test=false;
+    bool verbose=false, saveE=false, saveX=false, test=false, avg=false;
 
     uint coutprec = 12;
     cout.precision(coutprec);
@@ -45,6 +63,7 @@ int main(int argc, char** argv)
     pp.GetValue(nev,"nbands");
     pp.GetValue(saveE,"saveE");
     pp.GetValue(saveX,"saveX");
+    pp.GetValue(avg,"avg");
 
     pp.GetValue(filename,"state",true);
     pp.GetValue(datafolder,"datafolder");
@@ -69,12 +88,9 @@ int main(int argc, char** argv)
     /// create model
     modptr pmod = CreateModel(pp,verbose);
 
-    uint d = pmod->GetLocalDim();
     auto H = pmod->GetLocalHam();
-    uint nsym = pmod->GetNSym();
 
-    if (Kvec.size()!=nsym) throw std::domain_error("K has wrong number of quantum numbers");
-    IKey K0(pmod->GetGroupObj()); /// zero quantum number, use later to check if we can fully invert mixed transfer ops
+    if (Kvec.size()!=pmod->GetNSym()) throw std::domain_error("K has wrong number of quantum numbers");
     IKey K(pmod->GetGroupObj(),Kvec);
     IKey QN(pmod->GetGroupObj(),QNvec);
 
@@ -105,6 +121,7 @@ int main(int argc, char** argv)
 
     /// create I2K object and check its compliance with the loaded state
     auto I2K = pmod->GetI2K(N,QNvec);
+    auto obs = pmod->GetObservables(obsnames);
 
     if (ALvec != I2K) throw std::runtime_error("AL not compatible with I2K from model");
     if (ARvec != I2K) throw std::runtime_error("AR not compatible with I2K from model");
@@ -112,19 +129,6 @@ int main(int argc, char** argv)
 
     CheckOrthoLRSqrt(ALvec,ARvec,Cvec);
 
-
-    /// apply global shift to state
-    if (glob_shift > N)
-    {
-        glob_shift = glob_shift%N;
-        cerr<<endl<<"0 < glob_shift < N, setting to "<<glob_shift<<endl<<endl;
-    }
-    if (glob_shift > 0)
-    {
-        shift(ALvec,glob_shift);
-        shift(ARvec,glob_shift);
-        shift(Cvec,glob_shift);
-    }
 
     for (const auto& it : Cvec)
     {
@@ -149,72 +153,44 @@ int main(int argc, char** argv)
         shift(Lvec,rel_shift);
     }
 
-    /// show quantum number sectors
+
+    /// apply global shift to state
+    if (glob_shift > N)
+    {
+        glob_shift = glob_shift%N;
+        cerr<<endl<<"0 < glob_shift < N, setting to "<<glob_shift<<endl<<endl;
+    }
+
     if (verbose)
     {
         cout<<"I2K:"<<endl;
         cout<<I2K<<endl;
-
-        cout<<"AL[0] outgoing"<<endl;
-        cout<<ALvec[0].GetMr()<<endl;
-
-        cout<<"AR[0] ingoing"<<endl;
-        cout<<ARvec[1].GetMl()<<endl;
     }
 
-    BlockDiagMat<IKey,Scalar> L(Lvec.back());
-    BlockDiagMat<IKey,Scalar> R(Rvec.back());
+//    /// measure observables
+//    auto obs = pmod->GetObservables(obsnames);
+//    if (!obs.empty())
+//    {
+//        Real p=1.;
+////        pp.GetValue(p,"p");
+//
+//        LMpass.clear();
+//        RMpass.clear();
+//        if (abs(p)<10*InvETol && K==K0)
+//        {
+//            LMpass = LM;
+//            RMpass = RM;
+//        }
+//
+//        MeasureExcitations(ALvec,ARvec,NLvec,LMpass,RMpass,Lvec,Rvec,obs,xdims,mtot,p,InvETol,verbose);
+//        cout<<std::string(100,'=')<<endl;
+//    }
 
-    /// check overlaps between AL and AR and get left and right eigenmatrices of T^R_L (obtain the ones of T^L_R by hermitian conjugation)
-    BlockMat<IKey,Scalar> LM,RM,LMpass,RMpass;
-    cout<<"mixed TM AL and AR"<<endl;
-    LRoverlaps(LM,RM,ALvec,ARvec,K,nev);
 
-    /// calculate ground state energy density to subtract from Hamiltonian
-    cout<<"== H "<<std::string(94,'=')<<endl;
-    cout<<"-- AL "<<std::string(94,'-')<<endl;
-    RVecType E0vL = MeasureObservables(H,ALvec,BlockDiagMatArray<IKey,Scalar>(),Rvec,true);
-    cout<<"-- AR "<<std::string(94,'-')<<endl;
-    RVecType E0vR = MeasureObservables(H,ARvec,Lvec,BlockDiagMatArray<IKey,Scalar>(),true);
-    double E0L = mean(E0vL);
-    double E0R = mean(E0vR);
-    double dE0 = E0L - E0R;
-    if (std::abs(dE0) > tol) throw std::domain_error("ground state energies from AL and AR differ by " + to_varstring(dE0));
+//    /// early bail out for testing
+//    if (test) return 0;
 
-    double E0 = 0.5*(E0L + E0R);
-    H -= E0*SpId<Real>(d,2); /// subtract ground state energy from Hamiltonian
-
-    /// calculate Null spaces for all AL
-    for (const auto& Ait : ALvec) NLvec.emplace_back(Nullspace(Ait,l));
-    /// determine block structure and possible symmetry sectors for the excitation DOF BlockMat X
-    std::vector<dimkeypair_vec<IKey> > xdims;
-    uint mtot = XDims(xdims,NLvec,ARvec,K);
-    if (!all(xdims)) throw std::domain_error("no excitations for this quantum number");
-
-    auto obs = pmod->GetObservables(obsnames);
-    if (!obs.empty())
-    {
-        Real p=0.5;
-        pp.GetValue(p,"p");
-
-        LMpass.clear();
-        RMpass.clear();
-        if (abs(p)<10*InvETol && K==K0)
-        {
-            LMpass = LM;
-            RMpass = RM;
-        }
-
-        MeasureExcitations(ALvec,ARvec,NLvec,LMpass,RMpass,Lvec,Rvec,obs,xdims,mtot,p,InvETol,verbose);
-        cout<<std::string(100,'=')<<endl;
-    }
-
-    /// early bail out for testing
-    if (test) return 0;
-
-    /// prepare output filenames/folders etc.
-
-    if (saveE || saveX)
+    if (!test && (saveE || saveX))
     {
         savefolder = GetUniquePath(datafolder + "/" + sstr.str());
         if (!mkdir(savefolder)) throw std::runtime_error("couldn't create "+savefolder);
@@ -230,15 +206,138 @@ int main(int argc, char** argv)
         else cerr<<"failed to save ALRN under "<<saveALRname<<endl;
     }
 
-    /// calculate B independent constants for applying effective Hamiltonian
-    IDiagArray HLtot,HRtot;
-    HeffConstants(HLtot,HRtot,ALvec,ARvec,L,R,H,InvETol,0,verbose);
-
     /// actually calculate excitations
     RMatType dE(np,nev,fill::zeros);
     std::vector<CMatType> X(np);
 
-    BlockMat<IKey,Complex> EBR, EHBL;
+
+/// TODO (valentin#1#2017-11-12): from here on distinguish between avg or single shot and loop accordingly
+    if (glob_shift > 0)
+    {
+        shift(ALvec,glob_shift);
+        shift(ARvec,glob_shift);
+        shift(Cvec,glob_shift);
+        shift(Lvec,glob_shift);
+        shift(Rvec,glob_shift);
+    }
+
+    CalculateExcitations(dE,X,ALvec,ARvec,Lvec,Rvec,H,pvec,K,nev,tol,InvETol,verbose,test,obs);
+
+
+    if (saveE)
+    {
+        string saveEname = sstr.str()+"_dE";
+        if (dE.save(Fullpath(saveEname,"bin",savefolder))) cout<<"saved dE under "<<saveEname<<endl;
+        else cerr<<"failed to save dE under "<<saveEname<<endl;
+
+    }
+
+//    if (saveX)
+//    {
+//        string tmpXname = sstr.str() + "_X" + std::to_string(n);
+//        std::vector<BlockMatArray<IKey,Complex> > Xvec;
+//        Xvec.reserve(nev);
+//        for (uint l=0; l<nev; ++l) Xvec.emplace_back(BlockMatArray<IKey,Complex>(X[n].col(l),xdims));
+//        if (save(Xvec,Fullpath(tmpXname,"bin",savefolder))) cout<<"X"<<n<<" saved under "<<tmpXname<<endl;
+//        else cerr<<"failed to save X"<<n<<" under "<<tmpXname<<endl;
+//    }
+
+
+    return 0;
+}
+
+template<typename VT, typename KT, typename HVT>
+void
+CalculateExcitations(RMatType& dE,
+                     std::vector<CMatType>& X,
+                     const MPSBlockMatArray<KT,VT>& ALvec,
+                     const MPSBlockMatArray<KT,VT>& ARvec,
+                     const BlockDiagMatArray<KT,VT>& Lvec,
+                     const BlockDiagMatArray<KT,VT>& Rvec,
+                     SparseOperator<HVT>& H,
+                     const RVecType& pvec,
+                     const KT& K,
+                     uint nev,
+                     Real tol,
+                     Real InvETol,
+                     bool verbose,
+                     bool test,
+                     const std::vector<RSpOp>& obs)
+{
+    uint N = ALvec.size();
+    uint np = pvec.n_elem;
+    KT K0(K.GetGroupObj());
+
+    /// show quantum number sectors
+    if (verbose)
+    {
+        cout<<"AL[0] outgoing"<<endl;
+        cout<<ALvec[0].GetMr()<<endl;
+
+        cout<<"AR[0] ingoing"<<endl;
+        cout<<ARvec[1].GetMl()<<endl;
+    }
+
+    BlockDiagMat<KT,Scalar> L(Lvec.back());
+    BlockDiagMat<KT,Scalar> R(Rvec.back());
+
+    /// check overlaps between AL and AR and get left and right eigenmatrices of T^R_L (obtain the ones of T^L_R by hermitian conjugation)
+    BlockMat<KT,Scalar> LM,RM,LMpass,RMpass;
+    cout<<"mixed TM AL and AR"<<endl;
+    LRoverlaps(LM,RM,ALvec,ARvec,K,nev);
+
+    /// calculate ground state energy density to subtract from Hamiltonian
+    cout<<"== H "<<std::string(94,'=')<<endl;
+    cout<<"-- AL "<<std::string(94,'-')<<endl;
+    RVecType E0vL = MeasureObservables(H,ALvec,BlockDiagMatArray<KT,Scalar>(),Rvec,true);
+    cout<<"-- AR "<<std::string(94,'-')<<endl;
+    RVecType E0vR = MeasureObservables(H,ARvec,Lvec,BlockDiagMatArray<KT,Scalar>(),true);
+    double E0L = mean(E0vL);
+    double E0R = mean(E0vR);
+    double dE0 = E0L - E0R;
+    if (std::abs(dE0) > tol) throw std::domain_error("ground state energies from AL and AR differ by " + to_varstring(dE0));
+
+    double E0 = 0.5*(E0L + E0R);
+    H -= E0*SpId<Real>(H.GetLocalDim(),H.GetNSites()); /// subtract ground state energy from Hamiltonian
+
+    /// calculate Null spaces for all AL
+    MPSBlockMatArray<KT,VT> NLvec;
+    for (const auto& Ait : ALvec) NLvec.emplace_back(Nullspace(Ait,l));
+    /// determine block structure and possible symmetry sectors for the excitation DOF BlockMat X
+    std::vector<dimkeypair_vec<KT> > xdims;
+    uint mtot = XDims(xdims,NLvec,ARvec,K);
+    if (!all(xdims)) throw std::domain_error("no excitations for this quantum number");
+
+
+    /// measure observables
+    if (!obs.empty())
+    {
+//        for (const auto& oit : obs) cout<<oit.GetName()<<endl;
+        Real p=1.;
+//        pp.GetValue(p,"p");
+
+        LMpass.clear();
+        RMpass.clear();
+        if (abs(p)<10*InvETol && K==K0)
+        {
+            LMpass = LM;
+            RMpass = RM;
+        }
+
+        MeasureExcitations(ALvec,ARvec,NLvec,LMpass,RMpass,Lvec,Rvec,obs,xdims,mtot,p,InvETol,verbose);
+        cout<<std::string(100,'=')<<endl;
+    }
+
+    if (test) return;
+    /// prepare output filenames/folders etc.
+
+
+    /// calculate B independent constants for applying effective Hamiltonian
+    IDiagArray HLtot,HRtot;
+    HeffConstants(HLtot,HRtot,ALvec,ARvec,L,R,H,InvETol,0,verbose);
+
+
+    BlockMat<KT,Complex> EBR, EHBL;
 
     /// We use them to control if we invert (1-T^L_R) and (1-T^R_L) fully or if we project out the dominant subspace
     /// if they're
@@ -253,6 +352,9 @@ int main(int argc, char** argv)
 
         LMpass.clear();
         RMpass.clear();
+        EBR.clear();
+        EHBL.clear();
+
         /// only project out the dominant subspace, if K=0 and p=0
         if (abs(p_act)<10*InvETol && K==K0)
         {
@@ -260,9 +362,7 @@ int main(int argc, char** argv)
             RMpass = RM;
         }
 
-        std::function<void (Complex*,Complex*)> Hfun =
-//        [&xdims,mtot,kfac,&ALvec,&ARvec,&Cvec,&NLvec,&LMpass,&RMpass,&H,&HLtot,&HRtot,tol,InvETol,verbose,&EBR,&EHBL](Complex* in, Complex* out) -> void
-        [&](Complex* in, Complex* out) -> void
+        std::function<void (Complex*,Complex*)> Hfun = [&](Complex* in, Complex* out) -> void
         {
             ApplyHeff(in,out,xdims,mtot,kfac,ALvec,ARvec,NLvec,LMpass,RMpass,H,HLtot,HRtot,InvETol,verbose,&EBR,&EHBL);
         };
@@ -276,37 +376,10 @@ int main(int argc, char** argv)
         cout<<"p = "<<pvec(n)<<" "<<N<<" pi: \t";
         for (uint k=0; k<dE.n_cols; ++k) cout<<dE(n,k)<<"\t";
         cout<<"("<<tts.toc()<<" s.)"<<endl;
-
-        if (saveX)
-        {
-            string tmpXname = sstr.str() + "_X" + std::to_string(n);
-            std::vector<BlockMatArray<IKey,Complex> > Xvec;
-            Xvec.reserve(nev);
-            for (uint l=0; l<nev; ++l) Xvec.emplace_back(BlockMatArray<IKey,Complex>(X[n].col(l),xdims));
-            if (save(Xvec,Fullpath(tmpXname,"bin",savefolder))) cout<<"X"<<n<<" saved under "<<tmpXname<<endl;
-            else cerr<<"failed to save X"<<n<<" under "<<tmpXname<<endl;
-        }
-
     }
+
     double tel=tt.toc();
-
-//    for (uint n=0; n<np; ++n)
-//    {
-//        cout<<"p = "<<pvec(n)<<" "<<N<<" pi: \t";
-//        for (uint k=0; k<dE.n_cols; ++k) cout<<dE(n,k)<<"\t";
-//        cout<<endl;
-//    }
-
-    if (saveE)
-    {
-        string saveEname = sstr.str()+"_dE";
-        if (dE.save(Fullpath(saveEname,"bin",savefolder))) cout<<"saved dE under "<<saveEname<<endl;
-        else cerr<<"failed to save dE under "<<saveEname<<endl;
-
-    }
-
     if (tel>60) cout<<"elapsed time: "<<tel/double(60)<<" min."<<endl;
     else cout<<"elapsed time: "<<tel<<" s."<<endl;
 
-    return 0;
 }
